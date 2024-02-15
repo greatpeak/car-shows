59e3f68e729ef87cc113a13a5c456b632cab8217
greatpeak <abatandivine@gmail.com>
2024-02-15 13:53:14 +0100

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import NavBar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+
+export const metadata: Metadata = {
+  title: "Car Hub",
+  description: "Discover the best cars in the world",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className="relative">
+        <NavBar />
+        {children}
+        <Footer/>
+      </body>
+    </html>
+  );
+}
